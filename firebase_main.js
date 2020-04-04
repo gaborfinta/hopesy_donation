9eb93c61412e7198c5e59d2fd070da23fcbd7133
Finta Gabor <fintagabor8@gmail.com>
2020-04-04 13:46:17 +0200

    Add firebase config to firebase_main.js

--- firebase_main.js
+++ firebase_main.js
@@ -1,5 +1,10 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const app = require('./app');
+const createApp = require('./app');
+const DataStoreFactory = require('./src/datastore/datastorefactory');
+
+admin.initializeApp(functions.config().firebase);
+
+const app = createApp(DataStoreFactory('firebase'));
 
 exports.donation = functions.https.onRequest(app);
