96460fb8590a8c39bc00ec499354701cdc888d7a
nagybalint <nagybalint000@gmail.com>
2020-04-05 05:28:00 +0200

    Add CORS support to donations API

--- src/controllers/donations.js
+++ src/controllers/donations.js
@@ -1,13 +1,21 @@
 const Donation = require('../models/donation');
 
+const cors = require('cors');
+
+const corsOptions = {
+    origin: true,
+}
+
 const getAll = (datastore) => async (req, res) => {
     const donations = await datastore.getAll();
+    res.header("Access-Control-Allow-Origin", "*");
     res.json(donations);
 }
 
 const getById = (datastore) => async (req, res) => {
     const { id } = req.params;
     const donation = await datastore.getById(id);
+    res.header("Access-Control-Allow-Origin", "*");
     res.json(donation);
 }
 
@@ -16,13 +24,16 @@ const save = (datastore) => async (req, res) => {
     let newDonation = {};
     donationAttributes.forEach(attribute => newDonation[attribute] = req.body[attribute]);
     const newId = await datastore.save(new Donation(newDonation));
+    res.header("Access-Control-Allow-Origin", "*");
     res.json({newId});
 }
 
 const addRoutes = (router, datastore) => {
-    router.get('/', getAll(datastore));
-    router.post('/', save(datastore));
-    router.get('/:id/', getById(datastore));
+    router.get('/', cors(corsOptions), getAll(datastore));
+    router.post('/', cors(corsOptions), save(datastore));
+    router.get('/:id/', cors(corsOptions), getById(datastore));
+    router.options('/', cors(corsOptions));
+    router.options('/:id/', cors(corsOptions));
 }
 
 module.exports = addRoutes;
